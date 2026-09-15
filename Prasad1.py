92d55f1879b9334e89fcb2d89abba5d639c62d40
Prasad Bairagi <prasadbairagi131@gmail.com>
2026-09-15 11:14:20 +0530

    updated

--- Prasad1.py
+++ Prasad1.py
@@ -1,3 +1,10 @@
-a=5
-b=5
-a+b
+correct_pass = "741852" 
+not_found = True
+
+while not_found:
+    passw = input("enter passs: ")
+    if passw == correct_pass:
+        break
+        # not_found = False
+
+print("password match")
